d09ed18b52bd0f87419dd0c8fc18eb18702ea5fb
Dinesh Singh Dhami <dineshsinghdhamid@gmail.com>
2025-12-28 18:50:44 +0545

    Initial commit for image uploader project

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .forms import ImageForm
+from .models import Image
+
+def home(request):
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)  # <-- FILES in all caps
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # redirect after successful upload
+
+    else:
+        form = ImageForm()  # create an empty form if not POST
+
+    img = Image.objects.all()  # get all uploaded images
+    return render(request, 'myapp/home.html', {'img': img, 'form': form})
